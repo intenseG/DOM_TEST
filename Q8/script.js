fab313ec77e0eb009d66cdb8a413602b22e0d734
intenseG <intenseg1515@gmail.com>
2021-06-16 08:27:38 +0900

    onclickイベントをjs側で処理するよう修正

--- Q8/script.js
+++ Q8/script.js
@@ -2,3 +2,6 @@ function clickButton() {
     var box = document.getElementById('box');
     box.setAttribute('class', 'active');
 }
+
+var button = document.getElementById('changeBtn');
+button.onclick = clickButton;
